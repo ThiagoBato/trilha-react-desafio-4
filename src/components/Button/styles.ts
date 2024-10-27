a183996071b1cf0c7fcc7592039e330855cde81d
Thiago Oliveira Vieira <thiago.ov@hotmail.com>
2024-10-27 15:32:17 -0300

    desafio concluido

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -1,17 +1,22 @@
 import styled from 'styled-components';
+import { ButtonHTMLAttributes } from 'react';
 
+interface ButtonContainerProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  disabled?: boolean;
+}
 
-export const ButtonContainer = styled.button`
-    width: 100%;
-    height: 42px;
-    background-color: #81259D;
-    color: #FFF;
+export const ButtonContainer = styled.button<ButtonContainerProps>`
+  width: 100%;
+  height: 42px;
+  background-color: #81259D;
+  color: #FFF;
+  border: 1px solid #81259D;
+  border-radius: 21px;
 
-    border: 1px solid #81259D;
-    border-radius: 21px;
+  opacity: ${({ disabled }) => (disabled ? 0.6 : 1)};
+  cursor: ${({ disabled }) => (disabled ? 'not-allowed' : 'pointer')};
 
-    &:hover {
-        opacity: 0.6;
-        cursor:pointer;
-    }
-`
+  &:hover {
+    opacity: ${({ disabled }) => (disabled ? 0.6 : 0.8)};
+  }
+`;
